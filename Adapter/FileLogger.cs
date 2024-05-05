262fdc956b0dc4ee434e2a43a354e84dcb3a38e8
Gebreyowhans <gebreyohans2005@gmail.com>
2024-05-04 21:32:38 -0400

    Add project files.

---
+++ Adapter/FileLogger.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class FileLogger<T> : FileStream, ILogger<T>
+    {
+        public FileLogger(string path) : base(path, FileMode.Append) { } 
+
+        public IDisposable? BeginScope<TState>(TState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            byte[] messageByteArray = new UTF8Encoding(true).GetBytes(state.ToString() + "\n");
+            Write(messageByteArray, 0, messageByteArray.Length);
+            Flush();
+        }
+    }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+//using Microsoft.Extensions.Logging;
+using System.Text;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new FileLogger<Program>("Log.txt");
+            logger.LogDebug("This is a new log message");
+
+        }
+    }
+}
